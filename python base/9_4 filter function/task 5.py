3ad61ed481a6aeed95b48dd1c78e10e5afe186eb
Siarhei Khomich <limixserega@gmail.com>
2024-03-19 16:19:23 +0300

    done task finish filters

--- python base/9_4 filter function/task 5.py
+++ python base/9_4 filter function/task 5.py
@@ -8,3 +8,29 @@
 Результат отобразить в виде строки email-адресов, записанных через пробел.
 https://stepik.org/lesson/567074/step/7?unit=561348
 """
+import string
+
+chars_db = string.ascii_lowercase + string.digits + '_'
+
+mails = input().split()
+
+
+def is_valid(mail):
+    if mail.count('@') != 1 or mail.count('.') < 1:
+        return False
+    username, domain = mail.split('@')
+    if '.' not in domain:
+        return False
+    domain_name, tld = domain.split('.')
+    if not all(char in chars_db for char in username):
+        return False
+    if not all(char in chars_db for char in domain_name):
+        return False
+    if not all(char in string.ascii_lowercase for char in tld):
+        return False
+    return True
+
+
+valid_mails = filter(is_valid, mails)
+
+print(' '.join(valid_mails))
