6a2e1e9e25e4937d63fdaad322650ebbed79c78d
Siarhei Khomich <limixserega@gmail.com>
2024-05-18 12:43:16 +0300

    last task done, new just created

---
+++ object oriented programming/initializer __init__ & finalizer __del__/task 10.py
@@ -0,0 +1,11 @@
+class Cell:
+    pass
+
+
+class GamePole:
+    pass
+
+
+around_mines, mine = 4, True
+
+c1 = Cell(around_mines, mine)
--- object oriented programming/initializer __init__ & finalizer __del__/task 9.py
+++ object oriented programming/initializer __init__ & finalizer __del__/task 9.py
@@ -1,3 +1,8 @@
+"""
+https://stepik.org/lesson/701975/step/11?unit=702076
+"""
+
+
 class ListObject:
     def __init__(self, data, next_obj=None):
         self.next_obj = next_obj
@@ -5,3 +10,13 @@ class ListObject:
 
     def lin(self, obj):
         self.next_obj = obj
+
+
+lst_in = ['1', '3', '3', '2',]
+
+head_obj = ListObject(lst_in[0])
+obj = head_obj
+for i in range(1, len(lst_in)):
+    obj_new = ListObject(lst_in[i])
+    obj.lin(obj_new)
+    obj = obj_new
