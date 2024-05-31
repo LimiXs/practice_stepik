185e7db0a19ff0717753e8ef29955a8a368aa8aa
Siarhei Khomich <limixserega@gmail.com>
2024-05-31 17:26:59 +0300

    folders refact + 1 task

---
+++ OOP/Access Modes & Properties/property/task 2.py
@@ -0,0 +1,25 @@
+#  https://stepik.org/lesson/701984/step/7?unit=702085
+class WindowDlg:
+    def __init__(self, title, width, height):
+        self.__title = title
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        self.__height = height
+
+    def show(self):
+        print(f"{self.__title}: {self.__width}, {self.__height}")
