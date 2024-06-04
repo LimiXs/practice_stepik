8fe88f755a9f6e3fcd7577c62bc963e038664552
Siarhei Khomich <limixserega@gmail.com>
2024-06-04 17:10:38 +0300

    new task property

--- OOP/Access Modes & Properties/property/task 4.py
+++ OOP/Access Modes & Properties/property/task 4.py
@@ -1,13 +1,35 @@
 #  https://stepik.org/lesson/701984/step/9?unit=702085
 class RadiusVector2D:
-    def __init__(self, x, y):
-        self.__x = x
-        self.__y = y
+    MIN_COORD = -100
+    MAX_COORD = 1024
+
+    def __init__(self, x=0, y=0):
+        self.__x = self._validate_coordinate(x)
+        self.__y = self._validate_coordinate(y)
 
     @property
     def x(self):
         return self.__x
 
+    @x.setter
+    def x(self, value):
+        if isinstance(value, (int, float)):
+            self.__x = self._validate_coordinate(value)
+
     @property
     def y(self):
         return self.__y
+
+    @y.setter
+    def y(self, value):
+        if isinstance(value, (int, float)):
+            self.__y = self._validate_coordinate(value)
+
+    def _validate_coordinate(self, value):
+        if isinstance(value, (int, float)) and self.MIN_COORD <= value <= self.MAX_COORD:
+            return value
+        return 0
+
+    @staticmethod
+    def norm2(vector):
+        return vector.x**2 + vector.y**2
---
+++ OOP/Access Modes & Properties/property/task 5.py
@@ -0,0 +1,3 @@
+#  https://stepik.org/lesson/701984/step/10?unit=702085
+class DecisionTree:
+    
