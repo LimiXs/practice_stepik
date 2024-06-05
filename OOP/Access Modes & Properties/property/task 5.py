e83991f375a58b1e78043d83c595c6636c8563dd
Siarhei Khomich <limixserega@gmail.com>
2024-06-05 16:46:36 +0300

    task in process

--- OOP/Access Modes & Properties/property/task 5.py
+++ OOP/Access Modes & Properties/property/task 5.py
@@ -1,3 +1,54 @@
 #  https://stepik.org/lesson/701984/step/10?unit=702085
 class DecisionTree:
-    
+    """
+    для описания вершин и листьев решающего дерева;
+    """
+    def __init__(self, root):
+        self.root = root
+
+    @classmethod
+    def predict(cls, root, x):
+        pass
+
+    @classmethod
+    def add_obj(cls, obj, node=None, left=True):
+        pass
+
+
+class TreeObj:
+    """
+    для работы с решающим деревом в целом.
+    """
+    def __init__(self, indx, value=None):
+        self.indx = indx
+        self.value = value
+        self.__left = None
+        self.__right = None
+
+    @property
+    def left(self):
+        return self.__left
+
+    @property
+    def right(self):
+        return self.__right
+
+    @left.setter
+    def left(self, obj):
+        self.__left = obj
+
+    @right.setter
+    def right(self, obj):
+        self.__right = obj
+
+
+root = DecisionTree.add_obj(TreeObj(0))
+v_11 = DecisionTree.add_obj(TreeObj(1), root)
+v_12 = DecisionTree.add_obj(TreeObj(2), root, False)
+DecisionTree.add_obj(TreeObj(-1, "будет программистом"), v_11)
+DecisionTree.add_obj(TreeObj(-1, "будет кодером"), v_11, False)
+DecisionTree.add_obj(TreeObj(-1, "не все потеряно"), v_12)
+DecisionTree.add_obj(TreeObj(-1, "безнадежен"), v_12, False)
+
+x = [1, 1, 0]
+res = DecisionTree.predict(root, x) # будет программистом
