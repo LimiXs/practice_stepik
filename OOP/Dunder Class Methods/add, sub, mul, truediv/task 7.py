e494b242a8ff0bb4778d6a849510457951842eee
LimiXs <102148513+LimiXs@users.noreply.github.com>
2025-01-29 01:54:29 +0300

    great task MaxPolling, end of dunder

---
+++ OOP/Dunder Class Methods/add, sub, mul, truediv/task 7.py
@@ -0,0 +1,18 @@
+# https://stepik.org/lesson/701989/step/12?unit=702090
+
+class MaxPooling:
+    def __init__(self, step=(2, 2), size=(2, 2)):
+        self.step = step
+        self.size = size
+
+    def __call__(self, *args, **kwargs):
+        result = []
+        size_matrix = len(args[0])
+
+        for elm in args:
+            if any(not isinstance(x, (int, float)) for x in elm) or len(elm) != size_matrix:
+                raise ValueError("Неверный формат для первого параметра matrix.")
+
+        for i in range(0, size_matrix, self.step[0]):
+
+        return result
